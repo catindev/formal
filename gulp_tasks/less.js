1afd971887cbd9ce3888175c9c59b0a4f77b5ff2
Vladimir Titskiy <catindev@gmail.com>
2016-01-26 10:22:15 +0600

    new: autoprefixer

--- gulp_tasks/less.js
+++ gulp_tasks/less.js
@@ -1,19 +1,24 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var cssnano = require('gulp-cssnano');
+var autoprefixer = require('gulp-autoprefixer');
 var rename = require("gulp-rename");
-var concat = require('gulp-concat');
+//var concat = require('gulp-concat');
 
 // bundle
 gulp.task('bundle', function() {
     return  gulp.src([ 'src/index.less' ])
     .pipe(less())
     .pipe(cssnano())
+    .pipe(autoprefixer({
+		browsers: ['last 2 versions'],
+		cascade: false
+	}))
     .pipe(rename('./formal.min.css'))
     .pipe(gulp.dest('build/'));
 });
 
-// all-in css
+// standalone modules
 gulp.task('modules', function() {
     return  gulp.src([ 'src/*.less', '!src/index.less' ])
     .pipe(less())
